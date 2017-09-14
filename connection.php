5d0d6f25672d64ecb423ca57adb76806cced9615
karolisgrunda <karolis.grunda@gmail.com>
2017-09-14 23:01:41 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php    
- $host='ec2-54-228-189-223.eu-west-1.compute.amazonaws.com:5432';
- $user='oppuonjijkykut';
- $password='84b2958f9a00492f2518fc9d8df1b21082a35d1340c74923324d46ea543cf269';
- $db='d67n8ms0oom0m5';
+ $host='';
+ $user='';
+ $password='';
+ $db='';
 
  $link = mysql_connect('192.168.1.5', 'nfqproject', 'nfqmsql');
  if (!$link) {
