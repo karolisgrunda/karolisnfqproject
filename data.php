88a38df5f59751e68df65164c9367c0ee524f31e
karolisgrunda <karolis.grunda@gmail.com>
2017-09-17 00:00:41 +0300

    visual working

--- data.php
+++ data.php
@@ -10,9 +10,13 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+  <div class="container">
+    <h4><a href="index.html">Grįžti į puslapį</a></h4>
+  </div>
   <?php
     $host = 'eu-cdbr-west-01.cleardb.com';
     $db   = 'heroku_fb27e899e9e6887';
@@ -27,40 +31,41 @@
     ];
     $pdo = new PDO($dsn, $user, $pass, $opt);
   ?>
-
-  <table class="table">
-   <thead>
-    <tr>
-      <th>Numeris</th>
-      <th>Vardas</th>
-      <th>Lytis</th>
-      <th>Svoris</th>
-      <th>Ugis</th>
-      <th>Telefonas</th>
-      <th>El. pastas</th>
-      <th>Norai</th>
-    </tr>
-  </thead>
-    <tbody>
-       <tr>
-        <?php
-                $stmt = $pdo->query('SELECT id, Name, Gender, Weight, Height, Phone, Mail, Comment FROM klientai');
-                while ($row = $stmt->fetch()){
-                  echo '<tr>' .
-                  '<td>' . $row['id'] . '</td>' .
-                  '<td>' . $row['Name'] . '</td>' .
-                  '<td>' . $row['Gender'] . '</td>' .
-                  '<td>' . $row['Weight'] . '</td>' .
-                  '<td>' . $row['Height'] . '</td>'.
-                  '<td>' . $row['Phone'] . '</td>' .
-                  '<td>' . $row['Mail'] . '</td>'.
-                  '<td>' . $row['Comment'] . '</td> </tr>' ;
-
-              };
-        ?>
+  <div class="container">
+    <table class="table">
+    <thead>
+      <tr>
+        <th>Numeris</th>
+        <th>Vardas</th>
+        <th>Lytis</th>
+        <th>Svoris</th>
+        <th>Ugis</th>
+        <th>Telefonas</th>
+        <th>El. pastas</th>
+        <th>Norai</th>
       </tr>
-    </tbody>
-  </table>
+    </thead>
+      <tbody>
+        <tr>
+          <?php
+                  $stmt = $pdo->query('SELECT id, Name, Gender, Weight, Height, Phone, Mail, Comment FROM klientai');
+                  while ($row = $stmt->fetch()){
+                    echo '<tr>' .
+                    '<td>' . $row['id'] . '</td>' .
+                    '<td>' . $row['Name'] . '</td>' .
+                    '<td>' . $row['Gender'] . '</td>' .
+                    '<td>' . $row['Weight'] . '</td>' .
+                    '<td>' . $row['Height'] . '</td>'.
+                    '<td>' . $row['Phone'] . '</td>' .
+                    '<td>' . $row['Mail'] . '</td>'.
+                    '<td>' . $row['Comment'] . '</td> </tr>' ;
+
+                };
+          ?>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
 </body>
 </html>
