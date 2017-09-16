14675992d18fec37958a1542a4616999190826a5
karolisgrunda <karolis.grunda@gmail.com>
2017-09-16 12:31:06 +0300

    Trying to connect database

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 /*Prisijungimas prie duomenų bazės*/
-$con = mysqli_connect("localhost","root","","nfqtest");
+$con = mysqli_connect("localhost","bc27a370ba310d","39f73031","heroku_fb27e899e9e6887");
 
 if (mysqli_connect_errno())
   {
